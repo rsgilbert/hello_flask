245bee40962af889a247d4a7269f3101d6048bca
SSG <gssenyonjo@gmail.com>
2022-12-19 21:02:14 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask 
+from markupsafe import escape 
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<h1>Hello, there darling how are you?</h1>"
+
+
+@app.route("/<name>")
+def hello(name):
+    return f"Hello, {escape(name)}"
+
+
+
+    

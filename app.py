4833dc9ee01b46a13c6e54cf00cd5a9f59f4f840
SSG <gssenyonjo@gmail.com>
2022-12-19 21:23:25 +0300

    Looked at setting up basic routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask 
+from flask import Flask, url_for, request
 from markupsafe import escape 
 
 app = Flask(__name__)
@@ -12,6 +12,36 @@ def hello_world():
 def hello(name):
     return f"Hello, {escape(name)}"
 
+@app.route('/posts/<int:post_id>')
+def show_post(post_id):
+    return f'Post {escape(post_id)}'
 
+@app.route('/projects/')
+def projects():
+    return 'projects page'
 
+@app.route('/about')
+def about():
+    return 'About page'
     
+@app.route('/login', methods=['GET'])
+def login():
+    if request.method == 'GET':
+        return 'Get not allowed for logins!'
+
+@app.post('/login')
+def login_post():
+    return 'I am logging you in...'
+
+
+@app.get('/sample-pic')
+def sample_pic():
+    return url_for('static', filename="region-dim.PNG")
+
+
+with app.test_request_context():
+    print(url_for('about'))
+    print(url_for('hello_world'))
+    print(url_for('show_post', post_id=5))
+
+

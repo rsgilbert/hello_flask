8abf72b783e3b026b7e604496fb6384ff99d0c47
SSG <gssenyonjo@gmail.com>
2022-12-19 21:40:52 +0300

    looked at uploading files to flask

--- app.py
+++ app.py
@@ -1,47 +1,23 @@
-from flask import Flask, url_for, request
+from flask import Flask, url_for, request, render_template
 from markupsafe import escape 
+from werkzeug.utils import secure_filename
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<h1>Hello, there darling how are you?</h1>"
 
 
 @app.route("/<name>")
 def hello(name):
-    return f"Hello, {escape(name)}"
-
-@app.route('/posts/<int:post_id>')
-def show_post(post_id):
-    return f'Post {escape(post_id)}'
-
-@app.route('/projects/')
-def projects():
-    return 'projects page'
-
-@app.route('/about')
-def about():
-    return 'About page'
-    
-@app.route('/login', methods=['GET'])
-def login():
-    if request.method == 'GET':
-        return 'Get not allowed for logins!'
-
-@app.post('/login')
-def login_post():
-    return 'I am logging you in...'
+    age = request.args.get('age', 'Not mentioned')
+    return render_template('hello.html', name=name, age=age)
 
 
-@app.get('/sample-pic')
-def sample_pic():
-    return url_for('static', filename="region-dim.PNG")
-
-
-with app.test_request_context():
-    print(url_for('about'))
-    print(url_for('hello_world'))
-    print(url_for('show_post', post_id=5))
-
-
+@app.post('/upload')
+def upload_file():
+    f = request.files['my_file']
+    print(f.filename)
+    print(f'secure {secure_filename(f.filename)}')
+    print(f)
+    f.save(f'./uploads/{secure_filename(f.filename)}')
+    return f.filename

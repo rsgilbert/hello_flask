aa6caa61683bf85da5e91057cbc17b33b87a3dda
SSG <gssenyonjo@gmail.com>
2022-12-20 06:58:09 +0300

    Completed flask quickstart

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
-from flask import Flask, redirect, abort, url_for, request, make_response, render_template
+from flask import Flask, flash, session, redirect, abort, url_for, request, make_response, render_template
 from markupsafe import escape 
 from werkzeug.utils import secure_filename
 
 
 app = Flask(__name__)
+app.secret_key = b'Annia'
+
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return f'Logged in as {session["username"]}'
+    return 'You are not logged in'
+
+@app.post("/login")
+def login():
+    session["username"] = request.form["username"]
+    return redirect(url_for("index"))
+
+
+@app.get("/login")
+def login_get():
+    return render_template("login.html")
+
 
 @app.get('/hello')
 def hello():
@@ -20,6 +39,8 @@ def sample_redirection():
 
 @app.errorhandler(401)
 def unauthorized(error):
+    flash('Login first please')
+    app.logger.warn('An attempt to access the page when unauthorized')
     resp = make_response(render_template('unauthorized.html'), 401)
     resp.headers['X-App'] = 'Flask'
     return resp
